ca8c586bf1b4dbeb421487f1f6a2068d4d7e6816
Ubuntu <ubuntu@ip-172-31-42-71.ec2.internal>
2022-10-11 05:39:20 +0000

    CS2

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,26 @@
+class TopController < ApplicationController
+    
+    def main
+        if session[:login_uid]
+            render "main"
+        else
+            render "login"
+        end
+    end
+    
+    def login
+        if params[:uid] =='kindai' and params[:pass] == 'sanriko'
+            session[:login_uid] = params[:uid]
+            redirect_to root_path
+        else
+            render 'error'
+        end
+        
+    end
+    
+    def logout
+        session.delete(:login_uid)
+        redirect_to root_path
+    end
+    
+end
